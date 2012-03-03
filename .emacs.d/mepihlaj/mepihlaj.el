1a17c12e210f73975c700bb0d2a69a9da8ceefd6
zonpantli <zonpantli@gmail.com>
2012-03-03 03:32:17 +0200

    clean custom emacs e.

--- .emacs.d/mepihlaj/mepihlaj.el
+++ .emacs.d/mepihlaj/mepihlaj.el
@@ -1,10 +1,3 @@
-;; Connect to SLIME from lein Clojure projects, launch repl
-;; (defun slime-clojure ()
-;;   (interactive)
-;;   (require 'slime)
-;;   (slime-setup '(slime-repl))
-;;   (slime-connect "localhost" 4005))
-
 (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
 
 (add-hook 'slime-repl-mode-hook
@@ -29,21 +22,15 @@
             (mapconcat 'identity (directory-files (format "%s/lib" cdt-dir) t ".jar$") ":"))))
 
 ;;; cljs browser repl as inferior lisp
-(setq inferior-lisp-program "browser-repl")
+(setq inferior-lisp-program "lein repl")
 (add-hook 'inferior-lisp-mode-hook (lambda () (paredit-mode +1)))
 
 ;;; cljs files
 (add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
-(setq inferior-lisp-program "lein repl")
-
-;; (set-variable 'inferior-lisp-buffer "*inferior-lisp*")
-;; (set-variable 'inferior-lisp-program "/usr/bin/java -cp /Users/mepihlaj/Library/Clojure/lib/clojure.jar:/Users/mepihlaj/Library/Clojure/lib/clojure-contrib-1.2.0.jar clojure.main")
-;; (menu-bar-mode t)
-;; (server-start)
 
 ;;; Geiser scheme
 (load-file "~/projects/scheme/geiser/elisp/geiser.el")
-;; (add-hook 'geiser-racket-mode-hook (lambda () (paredit-mode +1)))
+(add-hook 'geiser-racket-mode-hook (lambda () (paredit-mode +1)))
 (setq geiser-active-implementations '(racket))
 
 ;;; set smaller font . OS X
@@ -52,7 +39,6 @@
 ;;; undo-tree mode
 (require 'undo-tree)
 (global-undo-tree-mode)
-
 
 ;;; textmate mode https://github.com/tanoku/textmate.el
 (require 'textmate)
