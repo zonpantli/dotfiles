70b58f0f3184f3d5759ab1f08a8a843a31c334a1
zonpantli <zonpantli@gmail.com>
2012-04-04 12:40:11 +0300

    show ido matches on separate rows

--- .emacs.d/mepihlaj/mepihlaj.el
+++ .emacs.d/mepihlaj/mepihlaj.el
@@ -186,3 +186,9 @@
 ;; io inferior mode
 (add-to-list 'load-path "~/.emacs.d/mepihlaj/io-emacs")
 (require 'io-mode-inf)
+
+;; ido customizations
+;; Display ido results vertically, rather than horizontally
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(defun ido-disable-line-trucation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-trucation)
