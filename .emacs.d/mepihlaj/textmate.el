c2d684b60f153f8090ff82cde98281c750931b53
zonpantli <zonpantli@gmail.com>
2012-04-05 19:59:18 +0300

    add jar to textmate mode find in project excludes

--- .emacs.d/mepihlaj/textmate.el
+++ .emacs.d/mepihlaj/textmate.el
@@ -77,7 +77,7 @@
 (defvar *textmate-project-root* nil)
 (defvar *textmate-project-files* '())
 (defvar *textmate-gf-exclude* 
-  "/\\.|vendor|fixtures|tmp|log|build|\\.xcodeproj|\\.nib|\\.framework|\\.app|\\.pbproj|\\.pbxproj|\\.xcode|\\.xcodeproj|\\.bundle")
+  "/\\.|vendor|fixtures|tmp|log|build|\\.xcodeproj|\\.nib|\\.framework|\\.app|\\.pbproj|\\.pbxproj|\\.xcode|\\.xcodeproj|\\.bundle|\\.jar")
 
 (defvar *textmate-keybindings-list* `((textmate-next-line 
                                      [A-return]    [M-return])
