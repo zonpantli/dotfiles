f2fd2fa41f19ab7d68c57b0939474398a4a651c8
Miika Pihlaja <zonpantli@gmail.com>
2013-07-21 23:58:02 +0300

    update lein plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[lein-pprint "1.1.1"]
                   [lein-light "0.0.4"]
                   [lein-ritz "0.7.0"]
-                  [lein-simpleton "1.1.0"]]}}
+                  [lein-simpleton "1.1.0"]
+                  [lein-try "0.2.0"]}}
 
 
