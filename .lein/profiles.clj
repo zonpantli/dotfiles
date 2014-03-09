b337d1c25d244f23e89ccdee63b9a2696a8aaea5
Miika Pihlaja <zonpantli@gmail.com>
2014-03-09 11:34:56 +0200

    add plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,6 +2,10 @@
                   [lein-light "0.0.4"]
                   [lein-ritz "0.7.0"]
                   [lein-simpleton "1.1.0"]
-                  [lein-try "0.2.0"]]}}
+                  [lein-try "0.2.0"]
+                  [lein-immutant "1.2.0"]
+                  [lein-try "0.4.1"]
+                  [lein-ancient "0.5.4"]
+                  [jonase/eastwood "0.1.0"]]}}
 
 
