8bd3ac778873f816603fee6d2b8e2fda01d709e5
Miika Pihlaja <zonpantli@gmail.com>
2014-11-01 21:28:18 +0200

    update cider and other emacs packages

--- .emacs.d/elpa/color-theme-6.5.5/color-theme-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; color-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("color-theme-pkg.el" "color-theme.el")
-;;;;;;  (20307 24262 561247))
-
-;;;***
-
-(provide 'color-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; color-theme-autoloads.el ends here
