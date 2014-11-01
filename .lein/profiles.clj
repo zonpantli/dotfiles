ed5e2314bd444c0b6660f83b567dbed60b9bb89d
Miika Pihlaja <zonpantli@gmail.com>
2014-11-01 21:27:48 +0200

    updata lein plugins in user profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,7 @@
-{:user {:plugins [[lein-pprint "1.1.1"]
-                  [lein-light "0.0.4"]
-                  [lein-ritz "0.7.0"]
-                  [lein-simpleton "1.1.0"]
-                  [lein-try "0.2.0"]
-                  [lein-immutant "1.2.0"]
+{:user {:plugins [[lein-simpleton "1.3.0"]
+                  [lein-try "0.4.3"]
                   [lein-try "0.4.1"]
-                  [lein-ancient "0.5.4"]
-                  [jonase/eastwood "0.1.0"]]}}
+                  [lein-ancient "0.5.5"]
+                  [jonase/eastwood "0.1.4"]]}}
 
 
